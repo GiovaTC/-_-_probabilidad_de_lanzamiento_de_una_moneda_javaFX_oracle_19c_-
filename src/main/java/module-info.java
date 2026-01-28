3323bee5d41e80411cb3b15f919e68745462daf6
gtapiero <giovanny.tapiero@pi.edu.co>
2026-01-28 10:35:54 -0500

    commit - ConexionOracle .

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ module com.example.probabilidad {
     requires org.controlsfx.controls;
     requires net.synedra.validatorfx;
     requires org.kordamp.bootstrapfx.core;
+    requires java.sql;
 
     opens com.example.probabilidad to javafx.fxml;
     exports com.example.probabilidad;
